e6f1d3eb610861a7e51e9c328a559df7a785f1b6
marinadeletic <53802832+marinadeletic@users.noreply.github.com>
2020-10-28 13:10:58 +1100

    Create time.js

---
+++ js/time.js
@@ -0,0 +1,83 @@
+{
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+  "title": {"text":"Usage By Time","fontSize": 20},
+
+  "hconcat":[{
+    "data": {
+      "url": "https://raw.githubusercontent.com/marinadeletic/FIT3179/main/time_day.csv"},
+    "layer":[
+      {
+      "mark": "bar",
+      "encoding": {
+        "x": {
+          "field": "weekday",
+          "sort":["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+          },
+        "y": {
+          "aggregate": "average", 
+          "field": "n"},
+        "size":{"value":5},
+        "color":{"field":"weekday"}}
+      },
+          {
+      "mark": "circle",
+      "encoding": {
+        "x": {
+          "field": "weekday",
+          "sort":["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]},
+        "y": {
+          "aggregate": "average", 
+          "field": "n",
+          "title":"Average Usage"},
+        "size":{"value":200},
+        "color":{"field":"weekday"},
+        "tooltip":[
+          {"title":"Day","field":"weekday"},
+          {"title":"Number","field":"n","aggregate":"average", "format":".2d"}]}
+      }]
+    },
+    {
+      "data": {
+      "url": "https://raw.githubusercontent.com/marinadeletic/FIT3179/main/time_day.csv"},
+    "layer":[
+      {
+      "mark": {"type":"line","interpolate":"basis"},
+      "encoding": {
+        "x": {
+          "field": "starttime",
+          "type": "quantitative",
+          "sort":"x",
+          "title":"Hour of Day (24h)"
+          },
+        "y": {
+          "aggregate": "average", 
+          "field": "n"},
+        "size":{"value":2},
+        "color":{"field":"weekday"}}
+      },
+      {
+        "data":{ "values":{"morning":8}},
+        "mark":"rule",
+        "encoding": {
+          "x":{"field":"morning", "type":"quantitative"},
+          "size":{"value":45},
+          "opacity":{"value":0.2},
+          "color":{"value":"lightgrey"}
+          }
+      },
+          {
+        "data":{ "values":{"evening":17.5}},
+        "mark":"rule",
+        "encoding": {
+          "x":{"field":"evening", "type":"quantitative"},
+          "size":{"value":45},
+          "opacity":{"value":0.2},
+          "color":{"value":"lightgrey"}
+          }
+      }
+      ],
+      "resolve": {"axis": {"x":"shared"}}
+      }
+  
+    ]
+}
