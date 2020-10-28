c05c0d5a8f17eb2ebc0f0f6336175a6f1fd9e520
marinadeletic <53802832+marinadeletic@users.noreply.github.com>
2020-10-28 13:06:33 +1100

    Update hour.js

--- js/hour.js
+++ js/hour.js
@@ -1,9 +1,3 @@
-{
-  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-  "width": 500,
-  "height": 400,
-  "title": {"text":"Usage By Time","fontSize": 20},
-
   "data": {
     "url": "https://raw.githubusercontent.com/marinadeletic/FIT3179/main/time_day.csv"},
   "layer":[
@@ -23,12 +17,24 @@
       "color":{"field":"weekday"}}
     },
     {
-      "data":{ "values":{"morning":7}},
+      "data":{ "values":{"morning":8}},
       "mark":"rule",
       "encoding": {
-        "x":{"field":"morning"}
+        "x":{"field":"morning", "type":"quantitative"},
+        "size":{"value":45},
+        "opacity":{"value":0.2},
+        "color":{"value":"lightgrey"}
+        }
+    },
+        {
+      "data":{ "values":{"evening":17.5}},
+      "mark":"rule",
+      "encoding": {
+        "x":{"field":"evening", "type":"quantitative"},
+        "size":{"value":45},
+        "opacity":{"value":0.2},
+        "color":{"value":"lightgrey"}
         }
     }
     ],
-    "resolve": {"scale": {"x": "shared"}}
-}
+    "resolve": {"axis": {"x":"shared"}}
